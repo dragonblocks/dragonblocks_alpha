b811d568a143d7b3e3ebb2b7eb612d3a3e3b7bc4
Elias Fleckenstein <eliasfleckenstein@web.de>
2021-07-16 14:16:24 +0200

    Use out of source build in the snapshot script

--- snapshot.sh
+++ snapshot.sh
@@ -1,16 +1,17 @@
 #! /bin/bash
 mkdir .build
 cp -r * .build/
-cd .build/src
-rm -rf CMakeCache.txt
-if ! (cmake . -DCMAKE_BUILD_TYPE=Release && make clean && make -j$(nproc)); then
-    cd ../..
-    rm -rf .build
+cd .build/
+mkdir build
+cd build
+if ! (cmake -B . -S ../src -DCMAKE_BUILD_TYPE=Release && make clean && make -j$(nproc)); then
+	cd ../..
+	rm -rf .build
 	exit 1
 fi
 cp Dragonblocks DragonblocksServer ..
 cd ..
-rm -rf .git* deps src BUILDING.md snapshot.sh upload.sh DragonblocksAlpha-*.zip
+rm -rf .git* deps src build BUILDING.md snapshot.sh upload.sh DragonblocksAlpha-*.zip
 cd ..
 mv .build DragonblocksAlpha
 VERSION=`git tag --points-at HEAD`
